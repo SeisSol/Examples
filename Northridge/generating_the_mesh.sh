91f306d39f9f9cc5c6a374000f14c661d41e271f
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2020-09-28 18:11:58 +0200

    Northridge mesh

---
+++ Northridge/generating_the_mesh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+prefix=mesh_northridge
+# Generate the mesh using gmsh
+#for gmsh, see http://gmsh.info/#Download
+gmsh -3 $prefix.geo
+
+# Convert the mesh from msh to neu using gmsh2gambit
+#for gmsh2gambit, see https://github.com/SeisSol/SeisSol/tree/master/preprocessing/meshing/gmsh2gambit
+gmsh2gambit -i $prefix.msh -o $prefix.neu
+
+# Convert the mesh from neu to hdf5 using pumgen
+# for pumgen, see https://github.com/SeisSol/PUMGen/wiki/How-to-compile-PUMGen
+pumgen $prefix.neu
