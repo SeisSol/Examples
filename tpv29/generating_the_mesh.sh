4971410cdb6e709fe82c3e6d86be201cd9aefd80
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2020-09-30 17:48:25 +0200

    generating tpv29 mesh

---
+++ tpv29/generating_the_mesh.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Download the scec data and unzip the file
+wget https://strike.scec.org/cvws/download/tpv29_tpv30_geometry_25m_data.zip --no-check-certificate
+unzip tpv29_tpv30_geometry_25m_data.zip
+
+#convert to tpv29_tpv30
+python generate_mytopo_tpv29.py
+
+#generate the skin mesh
+gmsh -3 tpv29.geo
+
+#compile and run the gmsh_plane2topo
+gfortran gmsh_plane2topo.f90 -o gmsh_plane2topo
+./gmsh_plane2topo interpol_topo.in
+
