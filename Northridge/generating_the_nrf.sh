60d08f0d6405312072a4e9ee8e957eaaa6ce7d11
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2021-07-30 14:28:49 +0200

    update path

--- Northridge/generating_the_nrf.sh
+++ Northridge/generating_the_nrf.sh
@@ -9,8 +9,8 @@ sed  '3{N;s/\n//;}' nr6.70-s0000-h0000.txt > ${prefix}.srf
 sed -i "s/\r//g" ${prefix}.srf
 
 # Now upsample the srf file with this script:
-# https://github.com/SeisSol/SeisSol/blob/master/preprocessing/science/refine_srf.py
-python refine_srf.py ${prefix}.srf --spatial_zoom 10 --temporal_zoom 10
+# This assumes SeisSol/SeisSol has been clone in ~
+python ~/SeisSol/SeisSol/preprocessing/science/kinematic_models/refine_srf.py ${prefix}.srf --spatial_zoom 10 --temporal_zoom 10
 
 #Now we create the nrf file using:
 #build rconv following the procedure described at https://github.com/SeisSol/SeisSol/tree/master/preprocessing/science/rconv
