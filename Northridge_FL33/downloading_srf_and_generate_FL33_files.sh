6d20185d311a2621f02ceb345656201e2ca28957
Jeremy Wong <44199955+jwjeremy@users.noreply.github.com>
2021-11-16 06:44:35 -0800

    Update downloading_srf_and_generate_FL33_files.sh
    Updated with Thomas's comment

--- Northridge_FL33/downloading_srf_and_generate_FL33_files.sh
+++ Northridge_FL33/downloading_srf_and_generate_FL33_files.sh
@@ -7,10 +7,7 @@ wget http://hypocenter.usc.edu/research/SRF/nr6.70-s0000-h0000.txt
 #merge line 3 and 4
 sed  '3{N;s/\n//;}' nr6.70-s0000-h0000.txt > ${prefix}.srf
 
-# linux
-sed -i "s/\r//g" ${prefix}.srf
-# mac
-#sed -i '' 's/\r//g' ${prefix}.srf
+sed  -i.bak "s/\r//g" ${prefix}.srf && rm ${prefix}.bak
 
 # This assumes SeisSol/SeisSol has been clone in ~
 python ~/SeisSol/SeisSol/preprocessing/science/kinematic_models/generate_FL33_input_files.py --spatial_zoom 10 ${prefix}.srf --gen "+proj=tmerc +datum=WGS84 +k=0.9996 +lon_0=-118.5150 +lat_0=34.3440"
