80051bdb0000747651703653fad64de7b2eb695b
Vikas Kurapati <vikas.kurapati@tum.de>
2024-05-17 10:13:51 +0200

    Scaled down mesh

--- tpv5/generating_the_mesh.sh
+++ tpv5/generating_the_mesh.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 prefix=tpv5_f200m
 # Generate the mesh using gmsh
 #for gmsh, see http://gmsh.info/#Download
-gmsh -3 -algo hxt $prefix.geo
+gmsh -3 -algo hxt $prefix.geo -clscale 5.0
 
 # Convert the mesh from neu to hdf5 using pumgen
 # for pumgen, see https://github.com/SeisSol/PUMGen/wiki/How-to-compile-PUMGen
